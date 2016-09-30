eae946709d8fbec3dae14aacdb8821e6b3001b37
Morad Edwar <me@morad-edwar.com>
2016-09-30 18:43:55 +0200

    Adding API Helper class

---
+++ app/src/main/java/api/ApiHelper.java
@@ -0,0 +1,10 @@
+package api;
+
+/**
+ * Created by morxander on 9/30/16.
+ */
+
+public class ApiHelper {
+    public static String API_BASE_URL = "http://api.fyber.com/feed/v1/";
+    public static String OFFERS_PATH = "offers.json";
+}
